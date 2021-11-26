73cce3e8e7ea32f8fbbc17a9309fdf6dda70ffd0
muneebulislam <muneebulislam@hotmail.com>
2021-11-25 20:00:33 -0600

    Invoices linked and delete button implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;
